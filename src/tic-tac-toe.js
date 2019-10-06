3aff9a9b30f39d0d389958c9eb067cee8a7021f4
NoWifi4U <max.shamber@gmail.com>
2019-10-06 16:39:32 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,53 @@
 class TicTacToe {
     constructor() {
-
+        this.field = [[null,null,null],
+                      [null,null,null],
+                      [null,null,null]];
+        this.turn = true;
     }
 
     getCurrentPlayerSymbol() {
-
+        return (this.turn) ? 'x' : 'o';
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.field[rowIndex][columnIndex] === null){
+            this.field[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+            this.turn = !this.turn;
+        }
     }
 
     isFinished() {
-
+        return this.noMoreTurns() || this.getWinner() !== null;
     }
 
     getWinner() {
-
+        for (let i = 0; i < 3; i++){
+            if (this.field[i][0] !== null && this.field[i][0] === this.field[i][1] && this.field[i][1] === this.field[i][2]) return this.field[i][0];
+            if (this.field[0][i] !== null && this.field[0][i] === this.field[1][i] && this.field[1][i] === this.field[2][i]) return this.field[0][i];
+        }
+        if (this.field[0][0] !== null && this.field[0][0] === this.field[1][1] && this.field[1][1] === this.field[2][2]) return this.field[0][0];
+        if (this.field[0][2] !== null && this.field[0][2] === this.field[1][1] && this.field[1][1] === this.field[2][0]) return this.field[0][2];
+        return null;
     }
 
     noMoreTurns() {
-
+        for (let row of this.field){
+            for (let square of row){
+                if (square === null) return false;
+            }
+        }
+        return true;
     }
 
     isDraw() {
-
+        if (this.getWinner() !== null) return false;
+        if (this.noMoreTurns()) return true;
+        return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex];
     }
 }
 
